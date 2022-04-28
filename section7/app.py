729dc404267a7f5519c7ca00786f55e73c181035
Roy Christian Pery <thercpery@gmail.com>
2022-04-29 06:25:21 +0800

    finished section 7

--- section7/app.py
+++ section7/app.py
@@ -3,6 +3,7 @@ import os
 from flask import Flask, jsonify
 from flask_restful import Api
 from flask_jwt_extended import JWTManager
+from flask_migrate import Migrate
 from marshmallow import ValidationError
 from dotenv import load_dotenv
 
@@ -22,6 +23,7 @@ app.config["PROPAGATE_EXCEPTIONS"] = True
 app.secret_key = "jose"
 api = Api(app)
 jwt = JWTManager(app)
+migrate = Migrate(app, db)
 db.init_app(app)
 
 
